4d97f269d7b228b0b86adab65dd133bf65375777
Chloedunwoody <54686732+Chloedunwoody@users.noreply.github.com>
2021-02-23 21:46:33 -0500

    Initial commit
    intial

---
+++ Assets/cubeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int myNumber = 1;
+    //public int mySecondNumber = 10;
+    public float myFloatNumber = 5f;
+
+    void Start()
+    {
+        //Debug.Log("Started");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Updating...");
+        transform.Rotate(1, 0, 0);
+    }
+}
